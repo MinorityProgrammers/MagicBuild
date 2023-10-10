0879792c7ca1dd0161da00c75274def8994be952
Kuro Denjiro <kurodenjiro1@gmail.com>
2023-10-11 01:06:40 +0700

    Create welcome.jsx

---
+++ welcome.jsx
@@ -0,0 +1,35 @@
+return (
+  <>
+    <div class="px-4 py-5 my-5 text-center">
+      <p class="display-1">🪄</p>
+      <h1 class="display-5 fw-bold text-body-emphasis">
+        Generate front ends from smart contracts
+      </h1>
+      <div class="col-lg-6 mx-auto">
+        <p class="lead mb-4">
+          Dont spend time building front ends for your Web3 smart contract.
+          Generate front end code in one click! Now on NEAR Protocol
+        </p>
+        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+          <a href="#" type="button" class="btn btn-dark btn-lg px-4">
+            🎉Telegram
+          </a>
+          <a
+            href="https://twitter.com/MagicBuildAI"
+            type="button"
+            class="btn btn-dark btn-lg px-4"
+          >
+            🐥 Twitter
+          </a>
+          <a
+            href="https://near.social/mob.near/widget/ProfilePage?accountId=magicbuild.near"
+            type="button"
+            class="btn btn-dark btn-lg px-4"
+          >
+            🧙🏻‍♂️ Near Social
+          </a>
+        </div>
+      </div>
+    </div>
+  </>
+);
