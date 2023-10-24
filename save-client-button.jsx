a2296a66ba6fa393ef495300fd759c1848567c2a
Kuro Denjiro <kurodenjiro1@gmail.com>
2023-10-25 02:40:43 +0700

    fix modal

--- save-client-button.jsx
+++ save-client-button.jsx
@@ -1,10 +1,25 @@
-State.init({ clientName: "", clientContract: "", clientList: [], error });
+State.init({
+  clientName: "",
+  clientContract: props.clientContract ? props.clientContract : "",
+  clientList: props.clientList ? props.clientList : [],
+  displayModal: false,
+  error,
+});
 const onInputChangeClientName = ({ target }) => {
   State.update({ clientName: target.value });
 };
 const onInputChangeClientContract = ({ target }) => {
   State.update({ error: null });
   State.update({ clientContract: target.value });
+};
+const showModal = (e, type) => {
+  console.log(state.displayModal);
+  if (type == "show") {
+    State.update({ displayModal: true });
+  }
+  if (type == "close") {
+    State.update({ displayModal: false });
+  }
 };
 const saveClient = () => {
   if (state.clientName.length < 5) {
@@ -45,7 +60,9 @@ const saveClient = () => {
         };
         Social.set(saveData, {
           force: true,
-          onCommit: () => {},
+          onCommit: () => {
+            State.update({ displayModal: false });
+          },
           onCancel: () => {},
         });
       } else {
@@ -59,85 +76,91 @@ const saveClient = () => {
 };
 
 return (
-  <>
+  <div>
     <label></label>
     <button
-      data-bs-toggle="modal"
-      data-bs-target="#createClient"
       class="btn btn-dark form-control "
+      onClick={(e) => showModal(e, "show")}
     >
       Save Client
     </button>
-    <div
-      class="modal fade"
-      id="createClient"
-      tabindex="-1"
-      aria-labelledby="createClientLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5" id="createClientLabel">
-              Create Client
-            </h1>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div class="modal-body">
-            <div class="form-group">
-              <label>Name</label>
-              <input
-                class="form-control"
-                onChange={(e) => onInputChangeClientName(e)}
-              />
-            </div>
-            <div class="form-group">
-              <label>Address</label>
-              <input
-                class="form-control"
-                onChange={(e) => onInputChangeClientContract(e)}
-              />
-            </div>
-            <div class="form-group">
-              <label>Chain</label>
-              <select class="form-control">
-                <option selected>Near Chain</option>
-                <option disabled>Ethereum Chain</option>
-                <option disabled>AVAX Chain</option>
-              </select>
-            </div>
-            {!state.error && (
-              <small class="form-text text-muted">
-                A new Client will be created.
-              </small>
-            )}
+    {state.displayModal && (
+      <>
+        <div
+          style={{ display: "block" }}
+          className={`modal fade show`}
+          id="createClient"
+          tabindex="-1"
+          aria-labelledby="createClientLabel"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="createClientLabel">
+                  Create Client
+                </h1>
+                <button
+                  type="button"
+                  class="btn-close"
+                  onClick={(e) => showModal(e, "close")}
+                ></button>
+              </div>
+              <div class="modal-body">
+                <div class="form-group">
+                  <label>Name</label>
+                  <input
+                    class="form-control"
+                    onChange={(e) => onInputChangeClientName(e)}
+                  />
+                </div>
+                <div class="form-group">
+                  <label>Address</label>
+                  <input
+                    class="form-control"
+                    onChange={(e) => onInputChangeClientContract(e)}
+                  />
+                </div>
+                <div class="form-group">
+                  <label>Chain</label>
+                  <select class="form-control">
+                    <option selected>Near Chain</option>
+                    <option disabled>Ethereum Chain</option>
+                    <option disabled>AVAX Chain</option>
+                  </select>
+                </div>
+                {!state.error && (
+                  <small class="form-text text-muted">
+                    A new Client will be created.
+                  </small>
+                )}
 
-            {state.error && (
-              <p class="text-danger" role="alert">
-                {state.error}
-              </p>
-            )}
-          </div>
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-secondary"
-              data-bs-dismiss="modal"
-            >
-              Close
-            </button>
-            <button type="button" onClick={saveClient} class="btn btn-primary">
-              Create
-            </button>
+                {state.error && (
+                  <p class="text-danger" role="alert">
+                    {state.error}
+                  </p>
+                )}
+              </div>
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  onClick={(e) => showModal(e, "close")}
+                >
+                  Close
+                </button>
+                <button
+                  type="button"
+                  onClick={saveClient}
+                  class="btn btn-primary"
+                >
+                  Create
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  </>
+        <div class="modal-backdrop fade show"></div>
+      </>
+    )}
+  </div>
 );
-sa
