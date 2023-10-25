5c09409f8f66e11293d8d075008a6a3413d1d448
Kuro Denjiro <kurodenjiro1@gmail.com>
2023-10-26 01:54:32 +0700

    Update save and create new

--- save-client-button.jsx
+++ save-client-button.jsx
@@ -1,77 +1,101 @@
 State.init({
-  clientName: "",
+  clientName: props.clientName ? props.clientName : "",
+  clientId: props.clientId ? props.clientId : null,
   clientContract: props.clientContract ? props.clientContract : "",
-  clientList: props.clientList ? props.clientList : [],
+  clientList: [],
+  abi: props.abi ? props.abi : null,
   displayModal: false,
   error,
+  clicked: false,
 });
 const onInputChangeClientName = ({ target }) => {
   State.update({ clientName: target.value });
+  State.update({ clicked: false });
 };
 const onInputChangeClientContract = ({ target }) => {
-  State.update({ error: null });
+  State.update({ error: null, clicked: false });
   State.update({ clientContract: target.value });
 };
 const showModal = (e, type) => {
-  console.log(state.displayModal);
   if (type == "show") {
-    State.update({ displayModal: true });
+    State.update({ displayModal: true, clicked: false });
   }
   if (type == "close") {
     State.update({ displayModal: false });
   }
 };
-const saveClient = () => {
-  if (state.clientName.length < 5) {
-    State.update({
-      error: "Name requires more than 5 characters",
-    });
-  } else {
-    asyncFetch("https://rpc.near.org/", {
-      body: JSON.stringify({
-        method: "query",
-        params: {
-          request_type: "view_code",
-          account_id: state.clientContract,
-          finality: "final",
+const loadData = () => {
+  const clientList = Social.get(`${context.accountId}/magicbuild/clientList`);
+  if (clientList) {
+    const clientListData = JSON.parse(clientList);
+    State.update({ clientList: clientListData });
+  }
+};
+loadData();
+const saveClient = (e) => {
+  if (!state.clicked) {
+    State.update({ clicked: true });
+    if (state.clientName.length < 5) {
+      State.update({
+        error: "Name requires more than 5 characters",
+      });
+    } else {
+      asyncFetch("https://rpc.near.org/", {
+        body: JSON.stringify({
+          method: "query",
+          params: {
+            request_type: "view_code",
+            account_id: state.clientContract,
+            finality: "final",
+          },
+          id: 154,
+          jsonrpc: "2.0",
+        }),
+        headers: {
+          "Content-Type": "application/json",
         },
-        id: 154,
-        jsonrpc: "2.0",
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-      method: "POST",
-    }).then((res) => {
-      if (res.body.result.code_base64) {
-        const data = state.clientList;
-        const clientData = {
-          id: Date.now(),
-          name: state.clientName,
-          address: state.clientContract,
-          archived: false,
-          abi: null,
-        };
-        data.push(clientData);
-        const saveData = {
-          magicbuild: {
-            clientlist: data,
-          },
-        };
-        Social.set(saveData, {
-          force: true,
-          onCommit: () => {
-            State.update({ displayModal: false });
-          },
-          onCancel: () => {},
-        });
-      } else {
-        State.update({
-          error:
-            "Unable to save Account ID because the contract has not been deployed yet!",
-        });
-      }
-    });
+        method: "POST",
+      }).then((res) => {
+        if (res.body.result.code_base64) {
+          const data = state.clientList;
+          if (state.clientId) {
+            data.forEach((item, index) => {
+              if (item.clientId == state.clientId) {
+                data[index].abi = state.abi;
+                data[index].clientName = state.clientName;
+              }
+            });
+          } else {
+            const clientId = Date.now();
+            const clientData = {
+              clientId: clientId,
+              clientName: state.clientName,
+              address: state.clientContract,
+              archived: false,
+              abi: state.abi,
+            };
+            data.push(clientData);
+          }
+          const saveData = {
+            magicbuild: {
+              clientList: data,
+            },
+          };
+          Social.set(saveData, {
+            force: true,
+            onCommit: () => {
+              State.update({ displayModal: false });
+            },
+            onCancel: () => {},
+          });
+        } else {
+          State.update({
+            error:
+              "Unable to save Account ID because the contract has not been deployed yet!",
+          });
+        }
+      });
+    }
   }
 };
 
@@ -79,10 +103,10 @@ return (
   <div>
     <label></label>
     <button
-      class="btn btn-dark form-control "
+      class="btn btn-primary form-control "
       onClick={(e) => showModal(e, "show")}
     >
-      Save Client
+      {state.clientId ? "Save Client" : "Create Client"}
     </button>
     {state.displayModal && (
       <>
@@ -110,6 +134,7 @@ return (
                   <label>Name</label>
                   <input
                     class="form-control"
+                    value={state.clientName}
                     onChange={(e) => onInputChangeClientName(e)}
                   />
                 </div>
@@ -117,6 +142,7 @@ return (
                   <label>Address</label>
                   <input
                     class="form-control"
+                    value={state.clientContract}
                     onChange={(e) => onInputChangeClientContract(e)}
                   />
                 </div>
@@ -150,7 +176,8 @@ return (
                 </button>
                 <button
                   type="button"
-                  onClick={saveClient}
+                  disabled={state.clicked}
+                  onClick={(e) => saveClient(e)}
                   class="btn btn-primary"
                 >
                   Create
@@ -159,6 +186,7 @@ return (
             </div>
           </div>
         </div>
+
         <div class="modal-backdrop fade show"></div>
       </>
     )}
